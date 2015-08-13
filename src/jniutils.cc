5afe57ad029cb29544035f6d89fce6ac75b2eb1c
Volker Birk <vb@pep-project.org>
2015-08-13 17:46:36 +0200

    completing C++ message

--- src/jniutils.cc
+++ src/jniutils.cc
@@ -7,6 +7,21 @@
 
 namespace pEp {
     namespace JNIAdapter {
+        jclass findClass(JNIEnv *env, const char *classname)
+        {
+            jclass clazz = env->FindClass(classname);
+            assert(clazz);
+
+            if (clazz == NULL) {
+                jclass ex = env->FindClass("java/lang/NoClassDefFoundError");
+                assert(ex);
+                env->ThrowNew(ex, classname);
+                throw std::bad_cast();
+            }
+            
+            return clazz;
+        }
+
         jfieldID getFieldID(
                 JNIEnv *env,
                 const char *classname,
@@ -14,17 +29,9 @@ namespace pEp {
                 const char *signature
             )
         {
-            jclass engine = env->FindClass(classname);
-            assert(engine);
+            jclass clazz = findClass(env, classname);
 
-            if (engine == NULL) {
-                jclass ex = env->FindClass("java/lang/NoClassDefFoundError");
-                assert(ex);
-                env->ThrowNew(ex, classname);
-                throw std::bad_cast();
-            }
-
-            jfieldID field = env->GetFieldID(engine, fieldname, signature);
+            jfieldID field = env->GetFieldID(clazz, fieldname, signature);
             assert(field);
 
             if (field == NULL) {
@@ -35,6 +42,75 @@ namespace pEp {
             }
 
             return field;
+        }
+
+        jlong callLongMethod(
+                JNIEnv *env,
+                jobject obj,
+                const char *methodname
+            )
+        {
+            jclass clazz = env->GetObjectClass(obj);
+            assert(clazz);
+
+            jmethodID method = env->GetMethodID(clazz, methodname, "()J");
+            if (method == NULL) {
+                jclass ex = env->FindClass("java/lang/NoSuchMethodError");
+                assert(ex);
+                env->ThrowNew(ex, methodname);
+                throw std::invalid_argument(std::string(methodname));
+            }
+
+            return env->CallLongMethod(obj, method);
+        }
+
+        jint outOfMemory(JNIEnv *env)
+        {
+            jclass ex;
+            const char *ex_name = "java/lang/OutOfMemoryError";
+
+            ex = env->FindClass(ex_name);
+            assert(ex);
+            return env->ThrowNew(ex, ex_name);
+        }
+
+        jbyteArray from_string(JNIEnv *env, const char *str)
+        {
+            if (str && str[0]) {
+                bool isCopy;
+                jbyteArray _str = NewByteArray(env, strlen(str));
+                jbyte *b = GetByteArrayElements(env, str, &isCopy);
+                strcpy((char *)b, str);
+                ReleaseByteArrayElements(env, str, b, 0);
+                return _str;
+            }
+            else {
+                return NewByteArray(env, 0);
+            }
+        }
+
+        char *to_string(JNIEnv *env, jbyteArray str)
+        {
+            bool isCopy;
+            jbyte *b = GetByteArrayElements(env, str, &isCopy);
+            char *_str = strndup((char *)b, (size_t)GetArrayLength(env, str));
+            ReleaseByteArrayElements(env, str, b, JNI_ABORT);
+            return _str;
+        }
+
+        jobject from_timestamp(JNIEnv *env, timestamp *ts)
+        {
+            time_t t = timegm(ts);
+            jclass clazz = findClass(env, "java/util/Date");
+            jmethodID constructor = env->GetMethodID(clazz, "<init>", "(J)V");
+            assert(constructor);
+            return env->NewObject(clazz, constructor, (jlong) t);
+        }
+
+        timestamp *to_timestamp(JNIEnv *env, jobject date)
+        {
+            time_t t = (time_t) callLongMethod(env, date, "getTime");
+            return new_timestamp(t);
         }
     };
 };
--- src/jniutils.hh
+++ src/jniutils.hh
@@ -4,12 +4,43 @@
 
 namespace pEp {
     namespace JNIAdapter {
+        jclass findClass(JNIEnv *env, const char *classname);
+
         jfieldID getFieldID(
                 JNIEnv *env,
                 const char *classname,
                 const char *fieldname,
                 const char *signature
             );
+
+        jlong callLongMethod(
+                JNIEnv *env,
+                jobject obj,
+                const char *methodname
+            );
+
+        jint outOfMemory(JNIEnv *env);
+
+        jbyteArray from_string(JNIEnv *env, const char *str);
+        char *to_string(JNIEnv *env, jbyteArray str);
+
+        jobject from_stringlist(JNIEnv *env, stringlist_t *sl);
+        stringlist_t *to_stringlist(JNIEnv *env, jobject sl);
+
+        jobject from_stringpairlist(JNIEnv *env, stringpair_list_t *sl);
+        stringpair_list_t *to_stringpairlist(JNIEnv *env, jobject sl);
+
+        jobject from_timestamp(JNIEnv *env, timestamp *ts);
+        timestamp *to_timestamp(JNIEnv *env, jobject date);
+
+        jobject from_identity(JNIEnv *env, pEp_identity *ident);
+        pEp_identity *to_identity(JNIEnv *env, jobject ident);
+
+        jobject from_identitylist(JNIEnv *env, identity_list *il);
+        identity_list *to_identitylist(JNIEnv *env, jobject il);
+
+        jobject from_bloblist(JNIEnv *env, bloblist_t *bl);
+        bloblist_t *to_bloblist(JNIEnv *env, jobject bl);
     };
 };
 
