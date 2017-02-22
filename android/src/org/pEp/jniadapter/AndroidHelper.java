e91826acd10d51c6977c533ca4a421f138a9b6ca
Hussein Kasem <huss@pep-project.org>
2017-02-22 12:31:27 +0100

    Update engine version code to 1601

--- android/src/org/pEp/jniadapter/AndroidHelper.java
+++ android/src/org/pEp/jniadapter/AndroidHelper.java
@@ -29,7 +29,7 @@ public class AndroidHelper {
     private static File tmpDir;
         
     // TODO : Increment when needed.
-    public static int ENGINE_VERSION_CODE = 1600;
+    public static int ENGINE_VERSION_CODE = 1601;
 
     private static File shareDir;
 
