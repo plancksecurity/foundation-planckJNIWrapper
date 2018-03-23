b9ff17a94fea02efcd2f75c26304386b22ccf87f
Hussein Kasem <huss@pep-project.org>
2018-03-23 16:54:15 +0100

    Fix typo on version number

--- android/src/org/pEp/jniadapter/AndroidHelper.java
+++ android/src/org/pEp/jniadapter/AndroidHelper.java
@@ -29,7 +29,7 @@ public class AndroidHelper {
     private static File tmpDir;
         
     // TODO : Increment when needed.
-    public static int ENGINE_VERSION_CODE = 2583;
+    public static int ENGINE_VERSION_CODE = 2585;
 
     private static File shareDir;
 
