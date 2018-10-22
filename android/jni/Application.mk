b1af275145f0b4258ebd53cd60ffa42474c56165
Hussein Kasem <huss@pep-project.org>
2018-10-22 14:59:21 +0200

    Backout to return to gnupg 2.0.30: b483b6363c38

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_STL := c++_shared
+APP_STL := c++_static
 APP_OPTIM := debug
 APP_PLATFORM=android-18
 APP_ABI := armeabi-v7a
--- android/jni/org_pEp_jniadapter_AndroidHelper.cc
+++ android/jni/org_pEp_jniadapter_AndroidHelper.cc
@@ -25,7 +25,7 @@ JNIEXPORT jint JNICALL Java_org_pEp_jniadapter_AndroidHelper_nativeSetup
     gpgme_set_global_flag("debug", cdebugflag);
     env->ReleaseStringUTFChars(debugflag, cdebugflag);
     gpgme_set_global_flag ("disable-gpgconf", "");
-    gpgme_set_global_flag ("gpg-name", "gpg");
+    gpgme_set_global_flag ("gpg-name", "gpg2");
 
     return 0;
 }
