2f782d6ad319bb1098a94e05145ce663c51fec82
Hussein Kasem <huss@pep-project.org>
2018-10-23 13:53:42 +0200

    Update gradle versions and engine version

--- android/src/org/pEp/jniadapter/AndroidHelper.java
+++ android/src/org/pEp/jniadapter/AndroidHelper.java
@@ -29,7 +29,7 @@ public class AndroidHelper {
     private static File tmpDir;
         
     // TODO : Increment when needed.
-    public static int ENGINE_VERSION_CODE = 2779;
+    public static int ENGINE_VERSION_CODE = 3063;
 
     private static File shareDir;
 
