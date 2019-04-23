4b2f1d0512aa1d99f97804b92506d80ccb2b24b1
Hussein Kasem <huss@pep-project.org>
2019-04-23 17:00:47 +0200

    Update engine revision to 3539

--- android/src/org/pEp/jniadapter/AndroidHelper.java
+++ android/src/org/pEp/jniadapter/AndroidHelper.java
@@ -29,7 +29,7 @@ public class AndroidHelper {
     private static File tmpDir;
         
     // TODO : Increment when needed.
-    public static int ENGINE_VERSION_CODE = 3424;
+    public static int ENGINE_VERSION_CODE = 3539;
 
     private static File shareDir;
 
