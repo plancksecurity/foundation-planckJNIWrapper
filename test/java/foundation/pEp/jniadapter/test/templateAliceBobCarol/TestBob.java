e0a511d1fdfb26b130c7d4ae957ec5b85e1c1489
heck <heck@pep.foundation>
2020-05-28 23:36:46 +0200

    add Test template templateAliceBobCarol

---
+++ test/java/foundation/pEp/jniadapter/test/templateAliceBobCarol/TestBob.java
@@ -0,0 +1,32 @@
+package foundation.pEp.jniadapter.test.templateAliceBobCarol;
+import static foundation.pEp.jniadapter.test.framework.TestLogger.*;
+import foundation.pEp.jniadapter.test.framework.*;
+import foundation.pEp.jniadapter.test.utils.*;
+
+class TestBob {
+    public static void main(String[] args) throws Exception {
+        TestSuite.setVerbose(true);
+        TestSuite.setTestColor(TestUtils.TermColor.YELLOW);
+
+        new TestUnit<AdapterBaseTestContext>("Test Bob",new AdapterBaseTestContext() , ctx  -> {
+            // do stuff using the context
+            // Test FAILS on unhandled exception, otherwise SUCCESS
+
+            ctx.bob = ctx.engine.myself(ctx.bob);
+
+            if(ctx.bob.fpr == null) {
+                throw new RuntimeException();
+            }
+
+            for(int i=0; i < 1000; i++ ) {
+                log("Bob is waiting...");
+                TestUtils.sleep(1000);
+            }
+
+        }).add();
+
+        TestSuite.run();
+    }
+}
+
+
---
+++ test/java/foundation/pEp/jniadapter/test/templateAliceBobCarol/TestCarol.java
@@ -0,0 +1,32 @@
+package foundation.pEp.jniadapter.test.templateAliceBobCarol;
+import static foundation.pEp.jniadapter.test.framework.TestLogger.*;
+import foundation.pEp.jniadapter.test.framework.*;
+import foundation.pEp.jniadapter.test.utils.*;
+
+class TestCarol {
+    public static void main(String[] args) throws Exception {
+        TestSuite.setVerbose(true);
+        TestSuite.setTestColor(TestUtils.TermColor.RED);
+
+        new TestUnit<AdapterBaseTestContext>("Test Carol",new AdapterBaseTestContext() , ctx  -> {
+            // do stuff using the context
+            // Test FAILS on unhandled exception, otherwise SUCCESS
+
+//            ctx.carol = ctx.engine.myself(ctx.carol);
+
+//            if(ctx.carol.fpr == null) {
+//                throw new RuntimeException();
+//            }
+
+            for(int i=0; i < 1000; i++ ) {
+                log("Carol is waiting...");
+                TestUtils.sleep(1000);
+            }
+
+        }).add();
+
+        TestSuite.run();
+    }
+}
+
+
