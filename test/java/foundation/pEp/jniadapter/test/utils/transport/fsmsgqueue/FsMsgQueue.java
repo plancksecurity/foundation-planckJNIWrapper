aa0c20aa0a9448bad0dad01bcb4d4f564aba0f37
heck <heck@pep.foundation>
2020-06-17 22:56:40 +0200

    Replace javafx.pair

--- test/java/foundation/pEp/jniadapter/test/utils/transport/fsmsgqueue/FsMsgQueue.java
+++ test/java/foundation/pEp/jniadapter/test/utils/transport/fsmsgqueue/FsMsgQueue.java
@@ -9,7 +9,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.util.*;
 
-import javafx.util.Pair;
 
 
 public class FsMsgQueue implements Queue<String> {
@@ -274,4 +273,29 @@ public class FsMsgQueue implements Queue<String> {
 
 }
 
+class Pair<K, V> {
+    private K key = null;
+    private V value = null;
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public Pair(K f, V s) {
+        key = f;
+        value = s;
+    }
+}
 
