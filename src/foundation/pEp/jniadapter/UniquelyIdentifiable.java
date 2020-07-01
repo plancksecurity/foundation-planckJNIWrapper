b2b6e678b3c031bb3e63013559a5e326403c182b
heck <heck@pep.foundation>
2020-07-01 19:35:03 +0200

    UniquelyIdentifiable improvements

--- src/foundation/pEp/jniadapter/UniquelyIdentifiable.java
+++ src/foundation/pEp/jniadapter/UniquelyIdentifiable.java
@@ -1,11 +1,12 @@
 package foundation.pEp.jniadapter;
 import java.util.concurrent.atomic.AtomicLong;
 
-abstract class UniquelyIdentifiable {
-    static final AtomicLong NEXT_ID = new AtomicLong(1);
-    final long id = NEXT_ID.getAndIncrement();
+// Abstract here so you can only inherit from, but not instantiate
+abstract public class UniquelyIdentifiable {
+    private static final AtomicLong NEXT_ID = new AtomicLong(1);
+    private final long id = NEXT_ID.getAndIncrement();
 
-    public long getId() {
+    protected long getId() {
         return id;
     }
 }
