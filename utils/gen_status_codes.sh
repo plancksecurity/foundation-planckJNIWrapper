c0e2a9157803acda6202b68009dbca2bf4cbad32
heck <heck@pep.foundation>
2020-09-11 04:22:57 +0200

    CodeGen: New enhanced enums, now all enums got .toString(), all enums can have .getInternalStringValue() (This was needed to avoid mapping table in the app, for "casting" Ratings to Engine internal string values for X-Opts .e.g)

--- utils/gen_status_codes.sh
+++ utils/gen_status_codes.sh
@@ -6,14 +6,13 @@ if [ ! -f $1 ]; then
     echo -e "\e[1m\e[31mInput file not found!\e[0m"
 fi
 
-if [ -z "$2" ]; then
-    echo -e "\e[1m\e[31mNo output file supplied\e[0m"
-fi
-
-if [ "$#" -ne 2 ]; then
-    echo "Expected use is: $0 input_file output_file"
+if [ "$#" -ne 1 ]; then
+    echo "Expected use is: $0 input_file"
     exit 1
 fi
+
+STAT_FILE=status_list.yml2
+PASS_FILE=passphrase_status_list.yml2
 
 ################################################################################
 #                                Select GNU SED                                #
@@ -33,15 +32,17 @@ esac
 #              Transform input file PEP_STATUS to yml2 status                  #
 ################################################################################
 
-$SED -n '/} PEP_STATUS/q;p' $1 > $2
-$SED -i -n '/STATUS_OK/,$p' $2
-$SED -i -e 's/\(.*\)/\L\1/' $2
-$SED -i "-e s/    pep/        pEp/g" $2
-$SED -i s/=/\>/g $2
-$SED -i s/,//g $2
+$SED -n '/} PEP_STATUS/q;p' $1 > $STAT_FILE
+$SED -i -n '/STATUS_OK/,$p' $STAT_FILE
+$SED -i -e 's/\(.*\)/\L\1/' $STAT_FILE
+$SED -i "-e s/    pep/        pEp/g" $STAT_FILE
+$SED -i s/=/\>/g $STAT_FILE
+$SED -i s/,//g $STAT_FILE
 
+grep -i passphrase $STAT_FILE | sed -e 's/^/enumitem/' > $PASS_FILE
 
 ################################################################################
 #                                 Show results                                 #
 ################################################################################
-cat $2
+cat $STAT_FILE
+cat $PASS_FILE
