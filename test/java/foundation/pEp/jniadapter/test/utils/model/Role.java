5e43980b80edb327d284345ee6e240b06d43f72a
heck <heck@pep.foundation>
2021-05-02 14:26:44 +0200

    Tests: TestModel - Add emum Role

---
+++ test/java/foundation/pEp/jniadapter/test/utils/model/Role.java
@@ -0,0 +1,19 @@
+package foundation.pEp.jniadapter.test.utils.model;
+
+public enum Role {
+    ALICE() {
+        public String toString() {
+            return "alice";
+        }
+    },
+    BOB() {
+        public String toString() {
+            return "bob";
+        }
+    },
+    CAROL() {
+        public String toString() {
+            return "carol";
+        }
+    };
+}
