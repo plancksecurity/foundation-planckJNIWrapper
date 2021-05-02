6e63ae980ad72737b38ec2bcda1756958ee21a53
heck <heck@pep.foundation>
2021-05-02 14:26:54 +0200

    Tests: TestModel - Add emum Node

---
+++ test/java/foundation/pEp/jniadapter/test/utils/model/Node.java
@@ -0,0 +1,34 @@
+package foundation.pEp.jniadapter.test.utils.model;
+
+public enum Node {
+    NODE_A1() {
+        public String toString() {
+            return "node_a1";
+        }
+    },
+    NODE_A2() {
+        public String toString() {
+            return "node_a2";
+        }
+    },
+    NODE_B1() {
+        public String toString() {
+            return "node_b1";
+        }
+    },
+    NODE_B2() {
+        public String toString() {
+            return "node_b2";
+        }
+    },
+    NODE_C1() {
+        public String toString() {
+            return "node_c1";
+        }
+    },
+    NODE_C2() {
+        public String toString() {
+            return "node_c2";
+        }
+    };
+}
