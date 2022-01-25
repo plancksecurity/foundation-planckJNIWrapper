bf69057e72914dfcdecd68bfa0c484e8d1fab514
heck <heck@pep.foundation>
2022-01-25 17:06:24 +0100

    Build: Change build config to adhere to standard for all adapters
    The idea is to avoid depending to n locations for n dependencies. Install everything into a $PREFIX, and depend to $PREFIX only. simple.

--- test/java/foundation/pEp/jniadapter/test/speedtest/speedtest.sh
+++ test/java/foundation/pEp/jniadapter/test/speedtest/speedtest.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-ENGINE_LIB_PATH=$HOME/local-default/lib
+PREFIX=$HOME/local
 
 export HOME=../resources/per-user-dirs/alice
-export LD_LIBRARY_PATH=$ENGINE_LIB_PATH
-export DYLD_LIBRARY_PATH=$ENGINE_LIB_PATH
+export LD_LIBRARY_PATH=$PREFIX/lib
+export DYLD_LIBRARY_PATH=$PREFIX/lib
 
 cd ../../../../../
 java -enableassertions -Xcheck:jni -cp .:../../src -Djava.library.path=.:../../src foundation.pEp.jniadapter.test.speedtest.SpeedTest $@
