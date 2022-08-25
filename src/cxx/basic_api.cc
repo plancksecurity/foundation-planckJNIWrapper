3004e0d16f70d730acfdf3d50fa4cb14d3a73c18
heck <heck@pep.foundation>
2022-08-25 21:47:49 +0200

    #20 - add function config_enable_echo_in_outgoing_message_rating_preview()

--- src/cxx/basic_api.cc
+++ src/cxx/basic_api.cc
@@ -553,5 +553,19 @@ JNIEXPORT void JNICALL Java_foundation_pEp_jniadapter_Engine__1config_1enable_1e
     ::config_enable_echo_protocol(session(),static_cast<bool>(enable));
 }
 
+JNIEXPORT void JNICALL Java_foundation_pEp_jniadapter_Engine__1config_1enable_1echo_1in_1outgoing_1message_1rating_1preview(JNIEnv *env,
+        jobject obj,
+        jboolean enable)
+{
+    std::mutex *mutex_local = nullptr;
+    {
+        std::lock_guard<std::mutex> l(global_mutex);
+        pEpLog("called with lock_guard");
+        mutex_local = get_engine_java_object_mutex(env, obj);
+    }
+    std::lock_guard<std::mutex> l(*mutex_local);
+    ::config_enable_echo_in_outgoing_message_rating_preview(session(),static_cast<bool>(enable));
+}
+
 } // extern "C"
 
