a123d2e485f9cd234bce3476ed32d56190f4ffb7
Sascha Bacardit <saschabacardit@gmail.com>
2023-06-30 02:19:58 +0200

    CORE-29 fixed YML bug added return class
    YML doesn't handle return class generation (????), additionally assumes a multireturn will always have a return or create param, which is incorrect.

---
+++ src/java/foundation/pEp/jniadapter/group_create_Return.java
@@ -0,0 +1,10 @@
+package foundation.pEp.jniadapter;
+
+import java.util.Vector;
+
+public class group_create_Return {
+	public _Identity groupidentity;
+	public _Identity manager;
+	public Vector<_Identity> memberidentlist;
+	public _Group group;
+}
