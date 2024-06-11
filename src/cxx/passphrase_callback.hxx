88c62655b66112b028fb538679400197b70aeb85
ignaciogarcia <ig@pep.security>
2024-06-11 11:24:31 +0200

    CORE-450 Update callback

--- src/cxx/passphrase_callback.hxx
+++ src/cxx/passphrase_callback.hxx
@@ -23,13 +23,13 @@ template<typename... A> PEP_STATUS passphraseWrap(PEP_STATUS f(PEP_SESSION, A...
             pEpLog("none of the cached passphrases worked");
             if (retryCount < maxRetries) {
                 // call the app
-                char *_passphrase = passphraseRequiredCallback(status);
+                PassphraseCache::simple_cache_entry entry = passphraseRequiredCallback(status);
                 pEpLog("callback returned, config_passphrase() with new passphrase");
                 PEP_STATUS inner_status;
                 if (status == PEP_PASSPHRASE_FOR_NEW_KEYS_REQUIRED) {
-                    inner_status = ::config_passphrase_for_new_keys(session, true, _passphrase);
+                    inner_status = ::config_passphrase_for_new_keys(session, true, entry.passphrase.c_str()); // needs to be changed in core
                 } else {
-                    inner_status = ::config_passphrase(session, passphrase_cache.add(_passphrase));
+                    inner_status = ::config_passphrase(session, passphrase_cache.add(entry));
                 }
                 if (inner_status == PEP_OUT_OF_MEMORY) {
                     return inner_status;
