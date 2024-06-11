882536e24dc89160dd1415f7f0ab656b058d7bd6
ignaciogarcia <ig@pep.security>
2024-06-11 12:45:39 +0200

    CORE-450 Use passphrase_entry as return type

--- src/cxx/passphrase_callback.hxx
+++ src/cxx/passphrase_callback.hxx
@@ -26,10 +26,10 @@ template<typename... A> PEP_STATUS passphraseWrap(PEP_STATUS f(PEP_SESSION, A...
                 PassphraseCache::cache_entry entry = passphraseRequiredCallback(status);
                 pEpLog("callback returned, config_passphrase() with new passphrase");
                 PEP_STATUS inner_status;
-                if (status == PEP_PASSPHRASE_FOR_NEW_KEYS_REQUIRED) {
+                if (status == PEP_PASSPHRASE_FOR_NEW_KEYS_REQUIRED) { // this should never be called now
                     inner_status = ::config_passphrase_for_new_keys(session, true, entry.passphrase.c_str()); // needs to be changed in core
                 } else {
-                    inner_status = ::config_passphrase(session, passphrase_cache.add(entry));
+                    inner_status = ::config_passphrase(session, passphrase_cache.add(entry).passphrase.c_str());
                 }
                 if (inner_status == PEP_OUT_OF_MEMORY) {
                     return inner_status;
