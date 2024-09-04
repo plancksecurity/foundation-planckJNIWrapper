e598e1f537f493c496639ea79be658ab5e51c6c9
ignaciogarcia <ig@pep.security>
2024-09-04 13:19:19 +0200

    CORE-516 Remove apparently useless flags

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -26,7 +26,6 @@ $(warning ==== JNIADAPTER android.mk using BOTAN2 for pEpEngineSequoiaBackend)
 include $(CLEAR_VARS)
 LOCAL_MODULE := botan
 LOCAL_SRC_FILES := $(GPGBUILD)/$(TARGET_ARCH_ABI)/lib/libbotan-2.a
-LOCAL_CPPFLAGS += -O3
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -44,7 +43,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE     := pEpJNI
 LOCAL_STATIC_LIBRARIES := pEpEngine libetpan libuuid libiconv pEpAdapter pEpCxx11 jitterentropy botan libpep_engine_sequoia_backend
 LOCAL_CPP_FEATURES += exceptions
-LOCAL_CPPFLAGS += -std=c++14 -DANDROID_STL=c++_shared -DHAVE_PTHREADS -DDISABLE_SYNC -fuse-ld=lld -frtti -O3
+LOCAL_CPPFLAGS += -std=c++14 -DANDROID_STL=c++_shared -DHAVE_PTHREADS -DDISABLE_SYNC -fuse-ld=lld -frtti
 LOCAL_SRC_FILES  := \
           ../../src/cxx/foundation_pEp_jniadapter_AbstractEngine.cc \
           ../../src/cxx/foundation_pEp_jniadapter_Engine.cc \
